3156076051b1e4d0695f34a7db7291ec60f401bc
kaushik379 <120251318+kaushik379@users.noreply.github.com>
2026-01-13 13:55:55 +0530

    Add script to list EC2 instances using Boto3

---
+++ list_ec2.py
@@ -0,0 +1,20 @@
+import boto3
+
+def list_ec2_instances():
+    ec2 = boto3.client("ec2")
+
+    response = ec2.describe_instances()
+
+    for reservation in response["Reservations"]:
+        for instance in reservation["Instances"]:
+            instance_id = instance["InstanceId"]
+            state = instance["State"]["Name"]
+            instance_type = instance["InstanceType"]
+
+            print(f"InstanceId: {instance_id}")
+            print(f"State: {state}")
+            print(f"Type: {instance_type}")
+            print("-" * 40)
+
+if __name__ == "__main__":
+    list_ec2_instances()
